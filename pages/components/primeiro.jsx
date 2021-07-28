d92c4274eaed1518aed636b35140b15c050ed957
@carolinaaguera <carolaguerabr@gmail.com>
2021-07-28 15:29:13 -0300

    navegation navbar

---
+++ pages/components/primeiro.jsx
@@ -0,0 +1,8 @@
+export default function Primeiro(){
+    return(
+        <>
+            <h1>Carol Aguera</h1>
+            {2 * 3}
+        </>
+    );
+}
---
+++ pages/components/segundo.jsx
@@ -0,0 +1,5 @@
+export default function Segundo(){
+    return(
+        <h2>Segunda tela</h2>
+    );
+}
